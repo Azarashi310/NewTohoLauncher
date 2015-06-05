6837bb5bed55d7af62fa7feeda9ce27ca2bd3f26
kudryavka310 <kudryavka.azarashi@gmail.com>
2015-06-06 05:56:06 +0900

    backupの実装をはじめるとこ

---
+++ NewTHL2/NewTHL2/Backup.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace NewTHL2
+{
+    partial class Backup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.decideButton = new System.Windows.Forms.Button();
+            this.savedata_Label = new System.Windows.Forms.Label();
+            this.replay_Label = new System.Windows.Forms.Label();
+            this.screenShot_Label = new System.Windows.Forms.Label();
+            this.savedata_TextBox = new System.Windows.Forms.TextBox();
+            this.replay_Textbox = new System.Windows.Forms.TextBox();
+            this.screenShot_Textbox = new System.Windows.Forms.TextBox();
+            this.profile_Textbox = new System.Windows.Forms.TextBox();
+            this.profile_Label = new System.Windows.Forms.Label();
+            this.autoSave_TextBox = new System.Windows.Forms.TextBox();
+            this.autoSave_Label = new System.Windows.Forms.Label();
+            this.okubi_Textbox = new System.Windows.Forms.TextBox();
+            this.okubi_Label = new System.Windows.Forms.Label();
+            this.macro_TextBox = new System.Windows.Forms.TextBox();
+            this.macro_Label = new System.Windows.Forms.Label();
+            this.savedataBuckupFolder_Browse_button = new System.Windows.Forms.Button();
+            this.replayBackupFolder_Browse_Button = new System.Windows.Forms.Button();
+            this.screenShotBackupFolder_Browse_Button = new System.Windows.Forms.Button();
+            this.autoSaveBackUpFolder_Browse_Button = new System.Windows.Forms.Button();
+            this.profileBackupFolder_Browse_Button = new System.Windows.Forms.Button();
+            this.OkubiBackupFolder_Browse_Button = new System.Windows.Forms.Button();
+            this.macroBackupFolder_Browse_Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(60, 501);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(149, 49);
+            this.cancelButton.TabIndex = 0;
+            this.cancelButton.Text = "キャンセル";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // decideButton
+            // 
+            this.decideButton.Location = new System.Drawing.Point(369, 501);
+            this.decideButton.Name = "decideButton";
+            this.decideButton.Size = new System.Drawing.Size(149, 49);
+            this.decideButton.TabIndex = 1;
+            this.decideButton.Text = "決定";
+            this.decideButton.UseVisualStyleBackColor = true;
+            // 
+            // savedata_Label
+            // 
+            this.savedata_Label.AutoSize = true;
+            this.savedata_Label.Location = new System.Drawing.Point(12, 35);
+            this.savedata_Label.Name = "savedata_Label";
+            this.savedata_Label.Size = new System.Drawing.Size(62, 12);
+            this.savedata_Label.TabIndex = 2;
+            this.savedata_Label.Text = "セーブデータ";
+            // 
+            // replay_Label
+            // 
+            this.replay_Label.AutoSize = true;
+            this.replay_Label.Location = new System.Drawing.Point(12, 100);
+            this.replay_Label.Name = "replay_Label";
+            this.replay_Label.Size = new System.Drawing.Size(39, 12);
+            this.replay_Label.TabIndex = 3;
+            this.replay_Label.Text = "リプレイ";
+            // 
+            // screenShot_Label
+            // 
+            this.screenShot_Label.AutoSize = true;
+            this.screenShot_Label.Location = new System.Drawing.Point(12, 165);
+            this.screenShot_Label.Name = "screenShot_Label";
+            this.screenShot_Label.Size = new System.Drawing.Size(79, 12);
+            this.screenShot_Label.TabIndex = 4;
+            this.screenShot_Label.Text = "スクリーンショット";
+            // 
+            // savedata_TextBox
+            // 
+            this.savedata_TextBox.Location = new System.Drawing.Point(138, 32);
+            this.savedata_TextBox.Name = "savedata_TextBox";
+            this.savedata_TextBox.Size = new System.Drawing.Size(271, 19);
+            this.savedata_TextBox.TabIndex = 5;
+            // 
+            // replay_Textbox
+            // 
+            this.replay_Textbox.Location = new System.Drawing.Point(138, 97);
+            this.replay_Textbox.Name = "replay_Textbox";
+            this.replay_Textbox.Size = new System.Drawing.Size(271, 19);
+            this.replay_Textbox.TabIndex = 6;
+            // 
+            // screenShot_Textbox
+            // 
+            this.screenShot_Textbox.Location = new System.Drawing.Point(138, 162);
+            this.screenShot_Textbox.Name = "screenShot_Textbox";
+            this.screenShot_Textbox.Size = new System.Drawing.Size(271, 19);
+            this.screenShot_Textbox.TabIndex = 7;
+            // 
+            // profile_Textbox
+            // 
+            this.profile_Textbox.Location = new System.Drawing.Point(138, 292);
+            this.profile_Textbox.Name = "profile_Textbox";
+            this.profile_Textbox.Size = new System.Drawing.Size(271, 19);
+            this.profile_Textbox.TabIndex = 9;
+            // 
+            // profile_Label
+            // 
+            this.profile_Label.AutoSize = true;
+            this.profile_Label.Location = new System.Drawing.Point(12, 295);
+            this.profile_Label.Name = "profile_Label";
+            this.profile_Label.Size = new System.Drawing.Size(58, 12);
+            this.profile_Label.TabIndex = 8;
+            this.profile_Label.Text = "プロフィール";
+            // 
+            // autoSave_TextBox
+            // 
+            this.autoSave_TextBox.Location = new System.Drawing.Point(138, 227);
+            this.autoSave_TextBox.Name = "autoSave_TextBox";
+            this.autoSave_TextBox.Size = new System.Drawing.Size(271, 19);
+            this.autoSave_TextBox.TabIndex = 11;
+            // 
+            // autoSave_Label
+            // 
+            this.autoSave_Label.AutoSize = true;
+            this.autoSave_Label.Location = new System.Drawing.Point(12, 230);
+            this.autoSave_Label.Name = "autoSave_Label";
+            this.autoSave_Label.Size = new System.Drawing.Size(58, 12);
+            this.autoSave_Label.TabIndex = 10;
+            this.autoSave_Label.Text = "自動セーブ";
+            // 
+            // okubi_Textbox
+            // 
+            this.okubi_Textbox.Location = new System.Drawing.Point(138, 357);
+            this.okubi_Textbox.Name = "okubi_Textbox";
+            this.okubi_Textbox.Size = new System.Drawing.Size(271, 19);
+            this.okubi_Textbox.TabIndex = 13;
+            // 
+            // okubi_Label
+            // 
+            this.okubi_Label.AutoSize = true;
+            this.okubi_Label.Location = new System.Drawing.Point(12, 360);
+            this.okubi_Label.Name = "okubi_Label";
+            this.okubi_Label.Size = new System.Drawing.Size(65, 12);
+            this.okubi_Label.TabIndex = 12;
+            this.okubi_Label.Text = "御首頂戴帳";
+            // 
+            // macro_TextBox
+            // 
+            this.macro_TextBox.Location = new System.Drawing.Point(138, 418);
+            this.macro_TextBox.Name = "macro_TextBox";
+            this.macro_TextBox.Size = new System.Drawing.Size(271, 19);
+            this.macro_TextBox.TabIndex = 15;
+            // 
+            // macro_Label
+            // 
+            this.macro_Label.AutoSize = true;
+            this.macro_Label.Location = new System.Drawing.Point(12, 425);
+            this.macro_Label.Name = "macro_Label";
+            this.macro_Label.Size = new System.Drawing.Size(31, 12);
+            this.macro_Label.TabIndex = 14;
+            this.macro_Label.Text = "マクロ";
+            // 
+            // savedataBuckupFolder_Browse_button
+            // 
+            this.savedataBuckupFolder_Browse_button.Location = new System.Drawing.Point(461, 30);
+            this.savedataBuckupFolder_Browse_button.Name = "savedataBuckupFolder_Browse_button";
+            this.savedataBuckupFolder_Browse_button.Size = new System.Drawing.Size(75, 23);
+            this.savedataBuckupFolder_Browse_button.TabIndex = 16;
+            this.savedataBuckupFolder_Browse_button.Text = "参照";
+            this.savedataBuckupFolder_Browse_button.UseVisualStyleBackColor = true;
+            // 
+            // replayBackupFolder_Browse_Button
+            // 
+            this.replayBackupFolder_Browse_Button.Location = new System.Drawing.Point(461, 95);
+            this.replayBackupFolder_Browse_Button.Name = "replayBackupFolder_Browse_Button";
+            this.replayBackupFolder_Browse_Button.Size = new System.Drawing.Size(75, 23);
+            this.replayBackupFolder_Browse_Button.TabIndex = 17;
+            this.replayBackupFolder_Browse_Button.Text = "参照";
+            this.replayBackupFolder_Browse_Button.UseVisualStyleBackColor = true;
+            // 
+            // screenShotBackupFolder_Browse_Button
+            // 
+            this.screenShotBackupFolder_Browse_Button.Location = new System.Drawing.Point(461, 160);
+            this.screenShotBackupFolder_Browse_Button.Name = "screenShotBackupFolder_Browse_Button";
+            this.screenShotBackupFolder_Browse_Button.Size = new System.Drawing.Size(75, 23);
+            this.screenShotBackupFolder_Browse_Button.TabIndex = 18;
+            this.screenShotBackupFolder_Browse_Button.Text = "参照";
+            this.screenShotBackupFolder_Browse_Button.UseVisualStyleBackColor = true;
+            // 
+            // autoSaveBackUpFolder_Browse_Button
+            // 
+            this.autoSaveBackUpFolder_Browse_Button.Location = new System.Drawing.Point(461, 225);
+            this.autoSaveBackUpFolder_Browse_Button.Name = "autoSaveBackUpFolder_Browse_Button";
+            this.autoSaveBackUpFolder_Browse_Button.Size = new System.Drawing.Size(75, 23);
+            this.autoSaveBackUpFolder_Browse_Button.TabIndex = 19;
+            this.autoSaveBackUpFolder_Browse_Button.Text = "参照";
+            this.autoSaveBackUpFolder_Browse_Button.UseVisualStyleBackColor = true;
+            // 
+            // profileBackupFolder_Browse_Button
+            // 
+            this.profileBackupFolder_Browse_Button.Location = new System.Drawing.Point(461, 290);
+            this.profileBackupFolder_Browse_Button.Name = "profileBackupFolder_Browse_Button";
+            this.profileBackupFolder_Browse_Button.Size = new System.Drawing.Size(75, 23);
+            this.profileBackupFolder_Browse_Button.TabIndex = 20;
+            this.profileBackupFolder_Browse_Button.Text = "参照";
+            this.profileBackupFolder_Browse_Button.UseVisualStyleBackColor = true;
+            // 
+            // OkubiBackupFolder_Browse_Button
+            // 
+            this.OkubiBackupFolder_Browse_Button.Location = new System.Drawing.Point(461, 355);
+            this.OkubiBackupFolder_Browse_Button.Name = "OkubiBackupFolder_Browse_Button";
+            this.OkubiBackupFolder_Browse_Button.Size = new System.Drawing.Size(75, 23);
+            this.OkubiBackupFolder_Browse_Button.TabIndex = 21;
+            this.OkubiBackupFolder_Browse_Button.Text = "参照";
+            this.OkubiBackupFolder_Browse_Button.UseVisualStyleBackColor = true;
+            // 
+            // macroBackupFolder_Browse_Button
+            // 
+            this.macroBackupFolder_Browse_Button.Location = new System.Drawing.Point(461, 416);
+            this.macroBackupFolder_Browse_Button.Name = "macroBackupFolder_Browse_Button";
+            this.macroBackupFolder_Browse_Button.Size = new System.Drawing.Size(75, 23);
+            this.macroBackupFolder_Browse_Button.TabIndex = 22;
+            this.macroBackupFolder_Browse_Button.Text = "参照";
+            this.macroBackupFolder_Browse_Button.UseVisualStyleBackColor = true;
+            // 
+            // Backup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 562);
+            this.Controls.Add(this.macroBackupFolder_Browse_Button);
+            this.Controls.Add(this.OkubiBackupFolder_Browse_Button);
+            this.Controls.Add(this.profileBackupFolder_Browse_Button);
+            this.Controls.Add(this.autoSaveBackUpFolder_Browse_Button);
+            this.Controls.Add(this.screenShotBackupFolder_Browse_Button);
+            this.Controls.Add(this.replayBackupFolder_Browse_Button);
+            this.Controls.Add(this.savedataBuckupFolder_Browse_button);
+            this.Controls.Add(this.macro_TextBox);
+            this.Controls.Add(this.macro_Label);
+            this.Controls.Add(this.okubi_Textbox);
+            this.Controls.Add(this.okubi_Label);
+            this.Controls.Add(this.autoSave_TextBox);
+            this.Controls.Add(this.autoSave_Label);
+            this.Controls.Add(this.profile_Textbox);
+            this.Controls.Add(this.profile_Label);
+            this.Controls.Add(this.screenShot_Textbox);
+            this.Controls.Add(this.replay_Textbox);
+            this.Controls.Add(this.savedata_TextBox);
+            this.Controls.Add(this.screenShot_Label);
+            this.Controls.Add(this.replay_Label);
+            this.Controls.Add(this.savedata_Label);
+            this.Controls.Add(this.decideButton);
+            this.Controls.Add(this.cancelButton);
+            this.Name = "Backup";
+            this.Text = "バックアップフォルダの設定";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button decideButton;
+        private System.Windows.Forms.Label savedata_Label;
+        private System.Windows.Forms.Label replay_Label;
+        private System.Windows.Forms.Label screenShot_Label;
+        private System.Windows.Forms.TextBox savedata_TextBox;
+        private System.Windows.Forms.TextBox replay_Textbox;
+        private System.Windows.Forms.TextBox screenShot_Textbox;
+        private System.Windows.Forms.TextBox profile_Textbox;
+        private System.Windows.Forms.Label profile_Label;
+        private System.Windows.Forms.TextBox autoSave_TextBox;
+        private System.Windows.Forms.Label autoSave_Label;
+        private System.Windows.Forms.TextBox okubi_Textbox;
+        private System.Windows.Forms.Label okubi_Label;
+        private System.Windows.Forms.TextBox macro_TextBox;
+        private System.Windows.Forms.Label macro_Label;
+        private System.Windows.Forms.Button savedataBuckupFolder_Browse_button;
+        private System.Windows.Forms.Button replayBackupFolder_Browse_Button;
+        private System.Windows.Forms.Button screenShotBackupFolder_Browse_Button;
+        private System.Windows.Forms.Button autoSaveBackUpFolder_Browse_Button;
+        private System.Windows.Forms.Button profileBackupFolder_Browse_Button;
+        private System.Windows.Forms.Button OkubiBackupFolder_Browse_Button;
+        private System.Windows.Forms.Button macroBackupFolder_Browse_Button;
+    }
+}
